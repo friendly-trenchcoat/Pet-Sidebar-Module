1c12df0002eb71c63673e61f421046916226ef35
friendly-trenchcoat <tippywigwom@yahoo.com>
2018-11-24 01:33:05 -0600

    capitalize globals | enable no petpet

--- petmodule.user.js
+++ petmodule.user.js
@@ -112,14 +112,13 @@ Big Items Still To Do:
 .split("(")[1].split(")")[0]
 */
 
-var pets = JSON.parse(localStorage.getItem("pets"));
-if(!pets) pets = [];
-var stats = [];
-var settings = [];
-var timestamp = new Date().getTime();
+var PETS = JSON.parse(localStorage.getItem("PETS")) || [];
+var STATS = [];
+var SETTINGS = [];
+var TIMESTAMP = new Date().getTime();
 
 function main() {
-    // update stats data
+    // update STATS data
     if (document.URL.indexOf("quickref") != -1) QuickRef();
     else if (document.URL.indexOf("status") != -1 && ( document.URL.indexOf("training") != -1 || document.URL.indexOf("academy") != -1 ) ) Training();
     //else if (document.URL.indexOf("process_training") != -1) EndTraining();
@@ -133,7 +132,7 @@ function main() {
     if ($(".sidebar")[0]) addElements();
 
     // store final list of pets
-    localStorage.setItem("pets", JSON.stringify(pets));
+    localStorage.setItem("pets", JSON.stringify(PETS));
 }
 function addElements(){
     var activePet = $('.sidebarModule:first-child tbody');
@@ -143,10 +142,10 @@ function addElements(){
     activePet.children().eq(3).remove();
     activePet.children().eq(2).remove();
 
-    stats = JSON.parse(localStorage.getItem(activePetName));
+    STATS = JSON.parse(localStorage.getItem(activePetName));
 
     // replace image
-    activePet.children().eq(1).find('img').attr('src',stats[16]);
+    activePet.children().eq(1).find('img').attr('src',STATS[16]);
 
     // over-module
     var over = activePet.first().clone(true);
@@ -167,12 +166,12 @@ function addElements(){
     // add inactive pets
     var petname;
     var c=0;
-    for (var i=0; i<pets.length; i++) {
-        petname = pets[i];
+    for (var i=0; i<PETS.length; i++) {
+        petname = PETS[i];
         if (petname !== activePetName) {
             c += 1;
-            stats = JSON.parse(localStorage.getItem(petname));
-            activePet.first().append('<tr id="inactive_'+petname+'"><td class="inactivePet" style="position: relative; z-index: 99;"><a href="/quickref.phtml"><img src="'+stats[16]+'" width="150" height="150" border="0" style=""></a></td></tr>'); // add module
+            STATS = JSON.parse(localStorage.getItem(petname));
+            activePet.first().append('<tr id="inactive_'+petname+'"><td class="inactivePet" style="position: relative; z-index: 99;"><a href="/quickref.phtml"><img src="'+STATS[16]+'" width="150" height="150" border="0" style=""></a></td></tr>'); // add module
             activePet.first().append(CreateHTML(petname, 215+(155*c))); // add menu
             $('#inactive_'+petname).hover(function(){ // hovering over module exposes menu
                 $(this).next().stop(true).animate({width: '500px'}, 800);
@@ -186,43 +185,45 @@ function addElements(){
     document.body.appendChild(CreateCSS());
 }
 function QuickRef() {
+    // All data except age and exact stat numbers
     $('.contentModuleTable tbody').each(function(k,v) {
-        if(k%2 === 0) {
+        if(k%2 === 0) { // even indexed elements are the relevant ones
             var names = $(v).find('th').first().text();
-            var regex = new RegExp('(.+) with (.+) the (.+)');
-            names = names.match(regex);
+            var regex = new RegExp('(.+) with (.+) the ([^ ]+)');
+            var namesMatch = names.match(regex);
+            names = ((namesMatch) ? namesMatch : [0, names, "", ""]); // in case of no petpet
             var petname = names[1];
             var newpet = 0;
-            if( pets.indexOf(petname) < 0 ) { // if pet isn't in list, add it
-                pets.push(petname);
+            if( PETS.indexOf(petname) < 0 ) { // if pet isn't in list, add it
+                PETS.push(petname);
                 newpet = 1;
             }
             else {
-                stats = JSON.parse(localStorage.getItem(petname));
-                if(!stats) stats = [];
+                STATS = JSON.parse(localStorage.getItem(petname));
+                if(!STATS) STATS = [];
             }
 
             var lines = $(v).find('.pet_stats td');
-            stats[0]  = timestamp;
-            stats[1]  = $(lines).eq(0).text();  // species
-            stats[2]  = $(lines).eq(1).text();  // color
-            stats[3]  = $(lines).eq(6).text();  // mood
-            stats[4]  = $(lines).eq(7).text();  // hunger
-            stats[5]  = $(lines).eq(3).text();  // age
-            stats[6]  = $(lines).eq(4).text();  // level
-            stats[7]  = $(lines).eq(5).text();  // hp
-            stats[8]  = $(lines).eq(8).text();  // strength   // will need to add check for low-level (and for pet lookup too)
-            stats[9]  = $(lines).eq(9).text();  // defence
-            stats[10] = $(lines).eq(10).text(); // movement
-            stats[11] = $(lines).eq(11).text(); // intelligence
-            stats[12] = names[2];               // petpet name
-            stats[13] = names[3];               // petpet species
-            if(newpet) stats[14] = '';          // petpet age (can't be found here)
-            stats[15] = $(lines).eq(12).find('img').attr('src');             // petpet image
-            stats[16] = $(v).find('.pet_image').attr('style').split("'")[1]; // pet image
-            console.log(stats);
+            STATS[0]  = TIMESTAMP;
+            STATS[1]  = $(lines).eq(0).text();  // species
+            STATS[2]  = $(lines).eq(1).text();  // color
+            STATS[3]  = $(lines).eq(6).text();  // mood
+            STATS[4]  = $(lines).eq(7).text();  // hunger
+            STATS[5]  = $(lines).eq(3).text();  // age
+            STATS[6]  = $(lines).eq(4).text();  // level
+            STATS[7]  = $(lines).eq(5).text();  // hp
+            STATS[8]  = $(lines).eq(8).text();  // strength   // will need to add check for low-level (and for pet lookup too)
+            STATS[9]  = $(lines).eq(9).text();  // defence
+            STATS[10] = $(lines).eq(10).text(); // movement
+            STATS[11] = $(lines).eq(11).text(); // intelligence
+            STATS[12] = names[2];               // petpet name
+            STATS[13] = names[3];               // petpet species
+            if(newpet) STATS[14] = '';          // petpet age (can't be found here)
+            STATS[15] = $(lines).eq(12).find('img').attr('src');             // petpet image
+            STATS[16] = $(v).find('.pet_image').attr('style').split("'")[1]; // pet image
+            console.log(STATS);
 
-            localStorage.setItem(petname, JSON.stringify(stats));
+            localStorage.setItem(petname, JSON.stringify(STATS));
         }
     });
 }
@@ -233,13 +234,13 @@ function Training() {
             // get name and retreive data (if any)
             var petname = $(v).children().first().text().split(" ")[0];
             var newpet = 0;
-            if( pets.indexOf(petname) < 0 ) { // if pet isn't in list, add it
-                pets.push(petname);
+            if( PETS.indexOf(petname) < 0 ) { // if pet isn't in list, add it
+                PETS.push(petname);
                 newpet = 1;
             }
             else {
-                stats = JSON.parse(localStorage.getItem(petname));
-                if(!stats) stats = [];
+                STATS = JSON.parse(localStorage.getItem(petname));
+                if(!STATS) STATS = [];
             }
 
             // get stats
@@ -247,20 +248,20 @@ function Training() {
             var regex = new RegExp('Lvl : (.+)Str : (.+)Def : (.+)Mov : (.+)Hp  : (.+)');
             dStats = dStats.match(regex);
 
-            stats[0]  = timestamp;
+            STATS[0]  = TIMESTAMP;
             if(newpet) {
-                for(var i=1; i<6; i++) stats[i] = '';
-                for(i=11; i<17; i++)   stats[i] = ''; // can actually get pet image here, but meh
+                for(var i=1; i<6; i++) STATS[i] = '';
+                for(i=11; i<17; i++)   STATS[i] = ''; // can actually get pet image here, but meh
             }
-            stats[6]  = dStats[1]; // level
-            stats[7]  = dStats[5]; // hp
-            stats[8]  = dStats[2]; //strengthString(dStats[2]); // strength
-            stats[9]  = dStats[3]; //defenceString(dStats[3]);  // defence
-            stats[10] = dStats[4]; //movementString(dStats[4]); // movement
-            console.log(stats);
+            STATS[6]  = dStats[1]; // level
+            STATS[7]  = dStats[5]; // hp
+            STATS[8]  = dStats[2]; //strengthString(dStats[2]); // strength
+            STATS[9]  = dStats[3]; //defenceString(dStats[3]);  // defence
+            STATS[10] = dStats[4]; //movementString(dStats[4]); // movement
+            console.log(STATS);
 
             // store data
-            localStorage.setItem(petname, JSON.stringify(stats));
+            localStorage.setItem(petname, JSON.stringify(STATS));
         }
     });
 }
@@ -271,30 +272,30 @@ function EndTraining() { // incomplete
 
     var petname = message[1];
     var newpet = 0;
-    if( pets.indexOf(petname) < 0 ) { // if pet isn't in list, add it
-        pets.push(petname);
+    if( PETS.indexOf(petname) < 0 ) { // if pet isn't in list, add it
+        PETS.push(petname);
         newpet = 1;
     }
     else {
-        stats = JSON.parse(localStorage.getItem(petname));
-        if(!stats) stats = [];
+        STATS = JSON.parse(localStorage.getItem(petname));
+        if(!STATS) STATS = [];
     }
 
     // get stats
-    stats[0]  = timestamp;
+    STATS[0]  = TIMESTAMP;
     if(newpet) {
-        for(var i=1; i<6; i++) stats[i] = '';
-        for(i=11; i<17; i++)   stats[i] = '';
+        for(var i=1; i<6; i++) STATS[i] = '';
+        for(i=11; i<17; i++)   STATS[i] = '';
     }
-    stats[6]  = dStats[1]; // level
-    stats[7]  = dStats[5]; // hp
-    stats[8]  = dStats[2]; //strengthString(dStats[2]); // strength
-    stats[9]  = dStats[3]; //defenceString(dStats[3]);  // defence
-    stats[10] = dStats[4]; //movementString(dStats[4]); // movement
-    console.log(stats);
+    STATS[6]  = dStats[1]; // level
+    STATS[7]  = dStats[5]; // hp
+    STATS[8]  = dStats[2]; //strengthString(dStats[2]); // strength
+    STATS[9]  = dStats[3]; //defenceString(dStats[3]);  // defence
+    STATS[10] = dStats[4]; //movementString(dStats[4]); // movement
+    console.log(STATS);
 
     // store data
-    localStorage.setItem(petname, JSON.stringify(stats));
+    localStorage.setItem(petname, JSON.stringify(STATS));
 }
 function FaerieQuest() {
     console.log("I'll get to it eventually.");
@@ -320,31 +321,31 @@ function Sidebar() {
     // get name and retreive data (if any)
     var petname = $("a[href='/quickref.phtml']").first().text();
     var newpet = 0;
-    if( pets.indexOf(petname) < 0 ) { // if pet isn't in list, add it
-        pets.push(petname);
+    if( PETS.indexOf(petname) < 0 ) { // if pet isn't in list, add it
+        PETS.push(petname);
         newpet = 1;
     }
     else {
-        stats = JSON.parse(localStorage.getItem(petname));
-        if(!stats) stats = [];
+        STATS = JSON.parse(localStorage.getItem(petname));
+        if(!STATS) STATS = [];
     }
 
     // get stats
     var activePetStats = $("td[align='left']");
-    stats[0]  = timestamp;
-    stats[1]  = $(activePetStats).eq(0).text();  // species
-    if(newpet) stats[2] = '';                    // color (can't be found here)
-    stats[3]  = $(activePetStats).eq(2).text();  // mood
-    stats[4]  = $(activePetStats).eq(3).text();  // hunger
-    stats[5]  = $(activePetStats).eq(4).text();  // age
-    stats[6]  = $(activePetStats).eq(5).text();  // level
-    stats[7]  = $(activePetStats).eq(1).text();  // hp
-    if(newpet) for(var i=8; i<16; i++) stats[i] = '';
-    stats[16] = $('.activePet a img').attr('src').slice(0,-5)+'4.png'; // pet image (use larger version)
-    console.log(stats);
+    STATS[0]  = TIMESTAMP;
+    STATS[1]  = $(activePetStats).eq(0).text();  // species
+    if(newpet) STATS[2] = '';                    // color (can't be found here)
+    STATS[3]  = $(activePetStats).eq(2).text();  // mood
+    STATS[4]  = $(activePetStats).eq(3).text();  // hunger
+    STATS[5]  = $(activePetStats).eq(4).text();  // age
+    STATS[6]  = $(activePetStats).eq(5).text();  // level
+    STATS[7]  = $(activePetStats).eq(1).text();  // hp
+    if(newpet) for(var i=8; i<16; i++) STATS[i] = '';
+    STATS[16] = $('.activePet a img').attr('src').slice(0,-5)+'4.png'; // pet image (use larger version)
+    console.log(STATS);
 
     // store data
-    localStorage.setItem(petname, JSON.stringify(stats));
+    localStorage.setItem(petname, JSON.stringify(STATS));
 }
 function Age() {
     console.log("I'll get to it eventually.");
@@ -386,7 +387,13 @@ function CreateCSS() { // 155 | 212 > 367 > 522 > 677 > 832
 }
 // [0 timestamp, 1 species, 2 color, 3 mood, 4 hunger, 5 age, 6 level, 7 health, 8 strength, 9 defence, 10 movement, 11 intelligence, 12 petpet name, 13 petpet species, 14 petpet age, 15 petpet image]
 function CreateHTML(petname, top) {
-    stats = JSON.parse(localStorage.getItem(petname));
+    STATS = JSON.parse(localStorage.getItem(petname));
+    var petpetTD = ((STATS[15]) ? // column for petpet if there is one
+        '<td align="center"> \
+        <b>'+STATS[12]+'</b> the '+STATS[13]+'<br><br> \
+        <img src="'+STATS[15]+'" width="80" height="80"><br><br> \
+        </td>'
+        : '' );
     var statsHTML = '\
     <div id="hover_'+petname+'" class="hover" style="position: absolute; "> \
     <div class="inner"> \
@@ -397,23 +404,23 @@ function CreateHTML(petname, top) {
     <table cellpadding="1" cellspacing="0" border="0"> \
     <tr> \
     <td align="right">Species:</td> \
-    <td align="left"><b>'+stats[1]+'</b></td> \
+    <td align="left"><b>'+STATS[1]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Color:</td> \
-    <td align="left"><b>'+stats[2]+'</b></td> \
+    <td align="left"><b>'+STATS[2]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Mood:</td> \
-    <td align="left"><b>'+stats[3]+'</b></td> \
+    <td align="left"><b>'+STATS[3]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Hunger:</td> \
-    <td align="left"><b>'+stats[4]+'</b></td> \
+    <td align="left"><b>'+STATS[4]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Age:</td> \
-    <td align="left"><b>'+stats[5]+'</b></td> \
+    <td align="left"><b>'+STATS[5]+'</b></td> \
     </tr> \
     </table> \
     </td> \
@@ -422,44 +429,64 @@ function CreateHTML(petname, top) {
     <table cellpadding="1" cellspacing="0" border="0"> \
     <tr> \
     <td align="right">Level:</td> \
-    <td align="left"><b>'+stats[6]+'</b></td> \
+    <td align="left"><b>'+STATS[6]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Health:</td> \
-    <td align="left"><b><b>'+stats[7]+'</b></b></td> \
+    <td align="left"><b><b>'+STATS[7]+'</b></b></td> \
     </tr> \
     <tr> \
     <td align="right">Strength:</td> \
-    <td align="left"><b>'+stats[8]+'</b></td> \
+    <td align="left"><b>'+STATS[8]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Defence:</td> \
-    <td align="left"><b>'+stats[9]+'</b></td> \
+    <td align="left"><b>'+STATS[9]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Movement:</td> \
-    <td align="left"><b>'+stats[10]+'</b></td> \
+    <td align="left"><b>'+STATS[10]+'</b></td> \
     </tr> \
     <tr> \
     <td align="right">Intelligence:</td> \
-    <td align="left"><b>'+stats[11]+'</b></td> \
+    <td align="left"><b>'+STATS[11]+'</b></td> \
     </tr> \
     </table> \
     </td> \
      \
-    <td align="center"> \
-    <b>'+stats[12]+'</b> the '+stats[13]+'<br><br> \
-    <img src="'+stats[15]+'" width="80" height="80"><br><br> \
-    </td> \
+    '+petpetTD+' \
      \
     </tr></table> \
      \
     </div> \
     </div>';
-    //    <i>'+stats[14]+'</i><br> \
+    //    <i>'+STATS[14]+'</i><br> \
     return statsHTML;
 }
 
+function _strengthString(word) {
+    var low = {'Not Yet Born':'0', 'Pathetic':'1','Very Weak':'2','Weak':'3-4','Frail':'5','Average':'6','Quite Strong':'7-9','Strong':'10-11','Very Strong':'12-13','Great':'14','Immense':'15-16','Titanic':'17-19','Herculean':'20'}
+    var n = ((word in low) ? low[word] : word.match(/\d+/g));
+    console.log("strength: ",word,n);
+    return n
+}
+function _defenceString(word) {
+    var low = {'Not Yet Born':'0','Defenceless':'1','Naked':'2','Vulnerable':'3','Very Poor':'4','Poor':'5','Below Average':'6-7','Average':'8','Armoured':'9','Tough':'10','Heavy':'11-12','Very Heavy':'13-14','Steel Plate':'15','Bullet Proof':'16','Semi Deadly Godly':'17','Demi Godly':'18','Godly':'19','Beyond Godly':'20'};
+    var n = ((word in low) ? low[word] : word.match(/\d+/g));
+    console.log("defense: ",word,n);
+    return n
+}
+function _movementString(word) {
+    var low = ['Not Yet Born','Barely Moves','Snail Pace','Lazy','Very Slow','Slow','Quite Slow','Average','Average','Fast','Speedy','Super Fast','Super Speedy','Breakneck','Cheetah','Lightening','Mach 1','Mach 1','Mach 2','Mach 3','Mach 4'];
+    var n = ((word.indexOf(low) < 0) ? word.match(/\d+/g) : word.indexOf(low));
+    n = ((n==7) ? '7-8' : n);
+    console.log("movement: ",word,n);
+    return n
+}
+function _intelligenceString(word) {
+    var low = {'Dim Witted':'0-4','Dull':'5-9','Average':'10-14','Above Average':'15-19','Bright':'20-24','Clever':'25-29','Very Clever':'30-34','Brilliant':'35-39','Genius':'40-44','Super Genius':'45-49','Mega Genius':'50-54','Total Genius':'55-59','Master Genius':'60-94'}
+    var n = ((word in low) ? low[word] : word.match(/\d+/g));
+}
 function strengthString(n) {
     var word;
     var low = ['Not Yet Born','Pathetic','Very Weak','Weak','Weak','Frail','Average','Quite Strong','Quite Strong','Quite Strong','Strong','Strong','Very Strong','Very Strong','Great','Immense','Immense','Titanic','Titanic','Titanic','Herculean'];
