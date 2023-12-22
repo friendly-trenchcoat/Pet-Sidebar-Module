6281fceba82fe6e7147e44eeaa017f465744a8c8
Friendly Trenchcoat <friendlytrenchcoat@gmail.com>
2023-12-22 01:02:22 -0500

    chore: :zap: add .meta.js file

---
+++ petmodule.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Neopets - Pets Sidebar Module
+// @namespace      https://github.com/friendly-trenchcoat
+// @version        2.0
+// @description    Customizable module displaying any number of pets for any number of accounts. Each pet has a navbar and stats info in menus which slide out on hover.
+// @author         friendly-trenchcoat
+// @match          https://www.neopets.com/*
+// @exclude        https://*.neopets.com/index.phtml
+// @exclude        https://*.neopets.com/login/*
+// @exclude        https://*.neopets.com/~*
+// @icon           https://www.google.com/s2/favicons?sz=64&domain=neopets.com
+// @grant          none
+// ==/UserScript==
